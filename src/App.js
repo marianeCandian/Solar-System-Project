04cd529368fa00ddf92b31ee352a448cf04cc820
Mariane Candian <mariane.candian@gmail.com>
2022-09-21 15:36:44 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <SolarSystem>
-          <Title titulo="Planetas" />
-        </SolarSystem>
+        <SolarSystem />
       </>
     );
   }
