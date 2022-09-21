3a0bf9559a28af527a824808e38eb9a2f38451f8
Mariane Candian <mariane.candian@gmail.com>
2022-09-21 14:47:34 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
+import './App.css';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (<Header />);
   }
 }
 
